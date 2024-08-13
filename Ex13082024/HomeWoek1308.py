32a1a5b9c7b9f1308c50549a16b8f759b9d5e588
Ramkumar <Pasupureddyramkumar@gmail.com>
2024-08-13 08:26:51 +0530

    added

---
+++ Ex13082024/HomeWoek1308.py
@@ -0,0 +1,12 @@
+# task :2
+num1 = int(input("enter a num1 : "))
+num2 = int(input("enter a num2 : "))
+print(max(num1, num2)) #max num
+print(pow(num1, num2)) #pow value
+print(num1 + num2) # sum
+print(num1 - num2) # sub
+print(num1 * num2) # multiply
+print(num1 / num2) # div
+
+a = "ram"
+print(f"{num1}", a)
